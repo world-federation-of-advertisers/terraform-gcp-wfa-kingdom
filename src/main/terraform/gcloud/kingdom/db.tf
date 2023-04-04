7989f8954eeae753fd7dc579ee266729010a4d84
wfa-siyegar <>
2023-04-04 09:29:23 +0530

    Added Kingdom

--- src/main/terraform/gcloud/kingdom/db.tf
+++ src/main/terraform/gcloud/kingdom/db.tf
@@ -19,6 +19,7 @@ resource "google_spanner_instance" "halo_spanner_db" {
   config       = "regional-${local.zone}"
   display_name = "${local.prefix}-spanner-instance"
   num_nodes    = local.spanner_db.num_nodes
+  deletion_protection = false
 }
 
 resource "google_spanner_database" "database" {
