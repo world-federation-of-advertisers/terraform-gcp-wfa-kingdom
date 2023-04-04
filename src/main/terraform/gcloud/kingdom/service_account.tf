747c4b76d492c8c2395ac8436f26720d3c5b87f1
wfa-siyegar <>
2023-04-04 10:03:47 +0530

    Added Kingdom

--- src/main/terraform/gcloud/kingdom/service_account.tf
+++ src/main/terraform/gcloud/kingdom/service_account.tf
@@ -52,8 +52,8 @@ resource "google_project_iam_binding" "gke_sa_iam_binding" {
 
 
 # Create Kubernetes service account
-resource "kubernetes_service_account" "internal_server" {
+/*resource "kubernetes_service_account" "internal_server" {
   metadata {
     name = "internal-server"
   }
-}
+}*/
